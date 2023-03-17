559fc4036180d25c34945901e49426993b073a55
btb331 <btb331@hotmail.com>
2023-03-17 21:57:31 +0000

    improving game data collection

--- inc/http/GridRivalClient.py
+++ inc/http/GridRivalClient.py
@@ -5,3 +5,42 @@ class GridRivalClient:
     def __init__(self, username:str, pwd:str) -> None:
         self.connect = GridRivalConnect()
         self.connect.login(username, pwd)
+    
+    def getGameData(self):
+        print("loading game data")
+        url = "v1/myleagues/statistics/elements"
+
+        payload = {
+            "element_type_k":"DRIVER",
+            "league_eid":1020032,
+            "season":"F1/23",
+            "sport_k":"F1"
+        }
+
+        data = self.connect.post(url, payload)
+
+        url = "v1/myleagues/statistics/elements"
+
+        payload = {
+            "element_type_k":"CONSTR",
+            "league_eid":1020032,
+            "season":"F1/23",
+            "sport_k":"F1"
+        }
+
+        teamData = self.connect.post(url, payload)
+
+        data['teamData'] = teamData['elements']
+
+        url = "v2/myleagues/league/card"
+
+        payload = {
+            "league_eid": "1020032"
+        }
+
+        cardData = self.connect.post(url, payload)
+
+        data['sports'] = cardData['sports']['F1'] #adding in extra data from other call
+
+        print("success")
+        return data
--- inc/http/GridRivalConnect.py
+++ inc/http/GridRivalConnect.py
@@ -5,7 +5,7 @@ class GridRivalConnect:
 
     def __init__(self) -> None:
         self.token: str = ""
-        self.baseUrl = "https://api.gridrival.com/api/v2/"
+        self.baseUrl = "https://api.gridrival.com/api/"
         pass
 
     def login(self, username:str, pwd:str) -> bool:
@@ -24,8 +24,30 @@ class GridRivalConnect:
             return False
         
         data = json.loads(resp.content)
-        self.token = data['token']
+        self.token = "Bearer " + data['token']
         print("success")
 
         return True
+    
+    def get(self, url:str):
+        resp = requests.get(self.baseUrl + url, headers={"content-type": "application/json;charset=UTF-8", "authorization": self.token})
+
+        if resp.status_code != 200:
+            print("Error getting ", url)
+            return False
+        return  json.loads(resp.content)
+    
+    def post(self, url:str, data: dict):
+
+        url = self.baseUrl+ url
+        data = json.dumps(data)
+
+
+        resp = requests.post(url, headers={"content-type": "application/json;charset=UTF-8", "authorization": self.token}, data= data)
+
+        if resp.status_code != 200:
+            print("Error posting to ", url)
+            print(resp.content)
+            return False
+        return  json.loads(resp.content)
 
