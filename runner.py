61f6cc583c66efc2a99d1fd5ed491e30f2b836ba
btb331 <btb331@hotmail.com>
2023-03-17 21:59:12 +0000

    runner now gets a driver

--- runner.py
+++ runner.py
@@ -1,6 +1,17 @@
 import json
 from inc.http.GridRivalClient import GridRivalClient
+from GridRivalFactory import GridRivalFactory
+from GridRival import GridRival
 
 loginData = json.load(open('login.json'))
 
 client = GridRivalClient(loginData['username'], loginData['password'])
+
+gridRivalFactory = GridRivalFactory(client)
+gridRivalFactory.load()
+
+gridRival:GridRival = gridRivalFactory.getGridRival()
+
+driver = gridRival.getDriver(937219)
+
+print(driver.totalPoints())
